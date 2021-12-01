cd1e26a95b2fa5861758c006d980c889829725c1
Jules Matsounga <jules.matsounga@bedrockstreaming.com>
2021-12-01 11:52:21 +0100

    chore: day1

---
+++ helper/file/read_text_file.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func ReadTextFile(path string, parser func(*bufio.Scanner) interface{}) interface{} {
+	f, err := os.Open("input.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	return parser(scanner)
+}
